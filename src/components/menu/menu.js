066e20ed4317ac467a19d864943e4fbe0ada497f
Kyron <kds060902@outlook.com>
2024-04-26 19:09:16 +0100

    setup nav links

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,8 +3,27 @@ import { Link } from 'gatsby';
 import useGetMenuItems from '../../graphql/useGetMenuItems';
 
 const Menu = () => {
-    const data = useGetMenuItems();
-    console.log(data);
-  };
+  const data = useGetMenuItems();
+
+  // Find the "Header Nav" menu
+  const headerNav = data && data.find(menu => menu.name === 'Header Nav');
+
+  return (
+    <nav>
+      <ul>
+        {headerNav && headerNav.menuItems.edges.map(({ node }) => {
+          const url = new URL(node.url);
+          const path = url.pathname;
+
+          return (
+            <li key={node.id}>
+              <Link to={path}>{node.label}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
