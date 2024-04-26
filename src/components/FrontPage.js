63aac4b58aee54780b86ceed4f8b28160640976f
Kyron <kds060902@outlook.com>
2024-04-26 20:27:33 +0100

    refactor front page

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import useGetFrontPage from '../graphql/useGetFrontPage';
 
 const FrontPage = () => {
@@ -7,20 +6,11 @@ const FrontPage = () => {
   
     // Find the object where isFrontPage is true
     const frontPageObject = data.find(item => item.isFrontPage);
-    
-  
-    // Access the blockBanner property
-    const blockBanner = frontPageObject ? frontPageObject.blockBanner : null;
   
     return (
-        <div>
-        {blockBanner && blockBanner.bannerImage && blockBanner.bannerImage.node ? (
-          <img
-            src={blockBanner.bannerImage.node.sourceUrl}
-            alt={blockBanner.bannerImage.node.altText}
-            style={{width: '100%'}}
-          />
-        ) : null}
+      <div>
+        <h1>{data.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
       </div>
     );
   };
