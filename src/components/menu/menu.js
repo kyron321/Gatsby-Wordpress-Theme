02061c16280cf133d898072968d6e601317955d3
Kyron <kds060902@outlook.com>
2024-04-26 20:21:26 +0100

    menu styling

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -9,14 +9,14 @@ const Menu = () => {
   const headerNav = data && data.find(menu => menu.name === 'Header Nav');
 
   return (
-    <nav>
-      <ul>
+    <nav className='header-nav'>
+      <ul className='menu-items'>
         {headerNav && headerNav.menuItems.edges.map(({ node }) => {
           const url = new URL(node.url);
           const path = url.pathname;
 
           return (
-            <li key={node.id}>
+            <li key={node.id} className='menu-item'>
               <Link to={path}>{node.label}</Link>
             </li>
           );
