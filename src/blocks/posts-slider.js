e87680d2b9fcb059203b6395d85d0c4a2b8648d6
Kyron <kds060902@outlook.com>
2024-05-09 21:51:17 +0100

    finalise desktop version of slider block

--- src/blocks/posts-slider.js
+++ src/blocks/posts-slider.js
@@ -3,8 +3,21 @@ import useGetPosts from '../graphql/useGetPosts';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { useStaticQuery, graphql } from 'gatsby';
 
 const PostsSlider = ({ attribs, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          siteUrl
+        }
+      }
+    }
+  `);
+
+  const siteUrl = data.site.siteMetadata.siteUrl;
+
   const childrenArray = React.Children.toArray(children);
   const registerPostSlider = childrenArray.find(child => child.type === 'register-wordpress-posts-slider');
 
@@ -16,22 +29,32 @@ const PostsSlider = ({ attribs, children }) => {
     speed: 100,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    swipe: false,
   };
 
   return (
-    <div className='post-container'>
-      <h2>Latest Posts</h2>
-      <Slider {...settings}>
-        {posts.slice(0, 4).map((post, index) => (
-          <div key={post.id || index} className="post-card">
-          <p>Posted on {new Date(post.date).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
-          {post.featuredImage && post.featuredImage.node && <img className="post-image" src={post.featuredImage.node.mediaItemUrl} alt={post.title} />}
-          <h2>{post.title}</h2>
-          <p>{post.excerpt}</p>
-        </div>
-        ))}
-      </Slider>
-    </div>
+    <section className="posts-slider-block">
+      <div className='post-slider-container'>
+        <h2 className='post-slider-title'>Latest Posts</h2>
+        <Slider {...settings}>
+          {posts.slice(0, 4).map((post, index) => {
+            const postUrl = new URL(post.uri, siteUrl);
+            postUrl.pathname = `/post${postUrl.pathname}`;
+            return (
+              <div key={post.id || index} className="post-card">
+                <a href={postUrl.href}>
+                  {post.featuredImage && post.featuredImage.node && <img className="post-image" src={post.featuredImage.node.mediaItemUrl} alt={post.title} />}
+                  <p>Posted on {new Date(post.date).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
+                  <h3>{post.title}</h3>
+                </a>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    </section>
   );
 };
 
--- src/graphql/useGetPosts.js
+++ src/graphql/useGetPosts.js
@@ -11,6 +11,7 @@ const useGetPosts = () => {
           date
           content
           slug
+          uri
           featuredImage {
             node {
               mediaItemUrl
