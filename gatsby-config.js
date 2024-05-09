58ddaf1299af5d38cb891fa323d321b281f0c12c
Kyron <kds060902@outlook.com>
2024-05-09 21:55:43 +0100

    make site url use env files

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `http://localhost:8000/`,
+    siteUrl: process.env.GATSBY_SITE_URL,
   },
   plugins: [
     {
