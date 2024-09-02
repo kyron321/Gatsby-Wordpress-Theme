0e25322abd539da6eb9f6963ea4399ee60528ddb
Kyron <kds060902@outlook.com>
2024-09-02 20:28:07 +0100

    implement star effect

--- src/blocks/star-canvas.js
+++ src/blocks/star-canvas.js
@@ -5,93 +5,204 @@ const StarCanvas = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    const canvasDots = function () {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      const colorDot = [
+        'rgb(81, 162, 233)',
+        'rgb(81, 162, 233)',
+        'rgb(81, 162, 233)',
+        'rgb(81, 162, 233)',
+        'rgb(228,128,70)',
+      ]; // 80% of dots are blue. 20% pink
+      const color = 'rgb(81, 162, 233)';
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+      canvas.width = document.body.scrollWidth;
+      canvas.height = window.innerHeight;
+      canvas.style.display = 'block';
+      ctx.lineWidth = 0.3;
+      ctx.strokeStyle = color;
 
-    const stars = [];
-    const FPS = 60;
-    const x = 100;
-    const mouse = { x: 0, y: 0 };
+      let mousePosition = {
+        x: (30 * canvas.width) / 100,
+        y: (30 * canvas.height) / 100,
+      };
 
-    for (let i = 0; i < x; i++) {
-      stars.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: Math.random() * 1 + 1,
-        vx: Math.floor(Math.random() * 50) - 25,
-        vy: Math.floor(Math.random() * 50) - 25
-      });
-    }
+      const windowSize = window.innerWidth;
+      let dots;
 
-    const draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.globalCompositeOperation = "lighter";
-
-      for (let i = 0; i < stars.length; i++) {
-        const s = stars[i];
-        ctx.fillStyle = "#fff";
-        ctx.beginPath();
-        ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.fillStyle = 'black';
-        ctx.stroke();
+      if (windowSize > 1600) {
+        dots = {
+          nb: 600, // number of particles
+          distance: 70, // max distance between particles for them to link
+          d_radius: 300, // radius from mouse location that particles will link
+          array: [],
+        };
+      } else if (windowSize > 1300) {
+        dots = {
+          nb: 575,
+          distance: 60,
+          d_radius: 280,
+          array: [],
+        };
+      } else if (windowSize > 1100) {
+        dots = {
+          nb: 500,
+          distance: 55,
+          d_radius: 250,
+          array: [],
+        };
+      } else if (windowSize > 800) {
+        dots = {
+          nb: 300,
+          distance: 0,
+          d_radius: 0,
+          array: [],
+        };
+      } else if (windowSize > 600) {
+        dots = {
+          nb: 200,
+          distance: 0,
+          d_radius: 0,
+          array: [],
+        };
+      } else {
+        dots = {
+          nb: 100,
+          distance: 0,
+          d_radius: 0,
+          array: [],
+        };
       }
 
-      ctx.beginPath();
-      for (let i = 0; i < stars.length; i++) {
-        const starI = stars[i];
-        ctx.moveTo(starI.x, starI.y);
-        if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-        for (let j = 0; j < stars.length; j++) {
-          const starII = stars[j];
-          if (distance(starI, starII) < 150) {
-            ctx.lineTo(starII.x, starII.y);
+      function Dot() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+
+        this.vx = -0.5 + Math.random();
+        this.vy = -0.5 + Math.random();
+
+        this.radius = Math.random() * 1.5;
+        this.colour = colorDot[Math.floor(Math.random() * colorDot.length)];
+      }
+
+      Dot.prototype = {
+        create: function () {
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+
+          const dotDistance =
+            ((this.x - mousePosition.x) ** 2 + (this.y - mousePosition.y) ** 2) **
+            0.5;
+          const distanceRatio = dotDistance / (windowSize / 1.7);
+
+          ctx.fillStyle = this.colour.slice(0, -1) + `,${1 - distanceRatio})`;
+          ctx.fill();
+        },
+
+        animate: function () {
+          for (let i = 1; i < dots.nb; i++) {
+            const dot = dots.array[i];
+
+            if (dot.y < 0 || dot.y > canvas.height) {
+              dot.vx = dot.vx;
+              dot.vy = -dot.vy;
+            } else if (dot.x < 0 || dot.x > canvas.width) {
+              dot.vx = -dot.vx;
+              dot.vy = dot.vy;
+            }
+            dot.x += dot.vx;
+            dot.y += dot.vy;
           }
+        },
+
+        line: function () {
+          for (let i = 0; i < dots.nb; i++) {
+            for (let j = 0; j < dots.nb; j++) {
+              const i_dot = dots.array[i];
+              const j_dot = dots.array[j];
+
+              if (
+                i_dot.x - j_dot.x < dots.distance &&
+                i_dot.y - j_dot.y < dots.distance &&
+                i_dot.x - j_dot.x > -dots.distance &&
+                i_dot.y - j_dot.y > -dots.distance
+              ) {
+                if (
+                  i_dot.x - mousePosition.x < dots.d_radius &&
+                  i_dot.y - mousePosition.y < dots.d_radius &&
+                  i_dot.x - mousePosition.x > -dots.d_radius &&
+                  i_dot.y - mousePosition.y > -dots.d_radius
+                ) {
+                  ctx.beginPath();
+                  ctx.moveTo(i_dot.x, i_dot.y);
+                  ctx.lineTo(j_dot.x, j_dot.y);
+
+                  const dotDistance =
+                    ((i_dot.x - mousePosition.x) ** 2 +
+                      (i_dot.y - mousePosition.y) ** 2) **
+                    0.5;
+                  let distanceRatio = dotDistance / dots.d_radius;
+
+                  distanceRatio -= 0.3;
+                  if (distanceRatio < 0) {
+                    distanceRatio = 0;
+                  }
+
+                  ctx.strokeStyle = `rgb(81, 162, 233, ${1 - distanceRatio})`;
+
+                  ctx.stroke();
+                  ctx.closePath();
+                }
+              }
+            }
+          }
+        },
+      };
+
+      function createDots() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < dots.nb; i++) {
+          dots.array.push(new Dot());
+          var dot = dots.array[i];
+
+          dot.create();
         }
+
+        dots.array[0].radius = 1.5;
+        dots.array[0].colour = '#';
+
+        dot.line();
+        dot.animate();
       }
-      ctx.lineWidth = 0.05;
-      ctx.strokeStyle = 'white';
-      ctx.stroke();
+
+      window.onmousemove = function (parameter) {
+        mousePosition.x = parameter.pageX;
+        mousePosition.y = parameter.pageY;
+
+        try {
+          dots.array[0].x = parameter.pageX;
+          dots.array[0].y = parameter.pageY;
+        } catch {
+          //
+        }
+      };
+
+      mousePosition.x = window.innerWidth / 2;
+      mousePosition.y = window.innerHeight / 2;
+
+      const draw = setInterval(createDots, 1000 / 30);
+
+      window.onresize = function () {
+        clearInterval(draw);
+        canvasDots();
+      };
     };
 
-    const distance = (point1, point2) => {
-      const xs = point2.x - point1.x;
-      const ys = point2.y - point1.y;
-      return Math.sqrt(xs * xs + ys * ys);
-    };
-
-    const update = () => {
-      for (let i = 0; i < stars.length; i++) {
-        const s = stars[i];
-        s.x += s.vx / FPS;
-        s.y += s.vy / FPS;
-        if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-        if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-      }
-    };
-
-    const tick = () => {
-      draw();
-      update();
-      requestAnimationFrame(tick);
-    };
-
-    canvas.addEventListener('mousemove', (e) => {
-      mouse.x = e.clientX;
-      mouse.y = e.clientY;
-    });
-
-    tick();
-
-    return () => {
-      canvas.removeEventListener('mousemove', () => {});
-    };
+    canvasDots();
   }, []);
 
-  return <canvas ref={canvasRef} id="canvas"></canvas>;
+  return <canvas ref={canvasRef} className="canvas-2"></canvas>;
 };
 
 export default StarCanvas;
