e8b5f67e12d4d3b32d43cdce9200b84b024028b7
Kyron <kds060902@outlook.com>
2024-09-02 21:24:43 +0100

    header and footer changes

--- src/components/header.js
+++ src/components/header.js
@@ -25,7 +25,7 @@ const Header = ({ siteTitle }) => {
   return (
     <header className={`header-container ${isScrolled ? 'scrolled' : ''}`}>
       <div className="header-inner">
-        <Link to="/">
+        {/* <Link to="/">
           {websiteOptions.textOrImageLogo ? (
             <h1 className="logo-text">
               {websiteOptions.siteTextLogo}
@@ -37,7 +37,7 @@ const Header = ({ siteTitle }) => {
               alt={websiteOptions.siteLogo.node.altText}
             />
           )}
-        </Link>
+        </Link> */}
         <Menu />
       </div>
     </header>
