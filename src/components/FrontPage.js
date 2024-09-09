95e32d7dd1ea0199b67d389a550104668ce6567a
Kyron <kds060902@outlook.com>
2024-09-09 16:47:47 +0100

    fix for scroll on laptop

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -48,29 +48,35 @@ const FrontPage = () => {
   useEffect(() => {
     // Add footer to sectionsRef
     sectionsRef.current.push('footer-container');
-
+  
+    let debounceTimeout;
+  
     const handleScroll = (event) => {
       event.preventDefault();
-      const delta = Math.sign(event.deltaY);
-      if (delta !== 0) {
-        currentSectionIndex.current = Math.min(
-          Math.max(currentSectionIndex.current + delta, 0),
-          sectionsRef.current.length - 1
-        );
-        const nextSection = document.querySelector(`.${sectionsRef.current[currentSectionIndex.current]}`);
-        if (nextSection) {
-          window.scrollTo({
-            top: nextSection.offsetTop,
-            behavior: 'smooth'
-          });
+      clearTimeout(debounceTimeout);
+      debounceTimeout = setTimeout(() => {
+        const delta = Math.sign(event.deltaY);
+        if (delta !== 0) {
+          currentSectionIndex.current = Math.min(
+            Math.max(currentSectionIndex.current + delta, 0),
+            sectionsRef.current.length - 1
+          );
+          const nextSection = document.querySelector(`.${sectionsRef.current[currentSectionIndex.current]}`);
+          if (nextSection) {
+            window.scrollTo({
+              top: nextSection.offsetTop,
+              behavior: 'smooth'
+            });
+          }
         }
-      }
+      }, 50); // Adjust the debounce delay as needed
     };
-
+  
     window.addEventListener('wheel', handleScroll, { passive: false });
-
+  
     return () => {
       window.removeEventListener('wheel', handleScroll);
+      clearTimeout(debounceTimeout);
     };
   }, []);
 
