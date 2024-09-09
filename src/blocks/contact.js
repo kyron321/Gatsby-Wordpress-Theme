a4c51ff2bff93cc808a5ed55666a0479554037f0
Kyron <kds060902@outlook.com>
2024-09-09 15:00:59 +0100

    changes to contact form

--- src/blocks/contact.js
+++ src/blocks/contact.js
@@ -26,9 +26,26 @@ const Contact = ({ attribs, children }) => {
 
     return (
         <section className="contact-container" id={sectionId} ref={sectionRef}>
-            test
+          <div className="contact-content">
+            <h2 className="contact-h2">Contact Me</h2>
+            <form action="" method="POST">
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div>
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" required></textarea>
+              </div>
+              <button type="submit">Send</button>
+            </form>
+          </div>
         </section>
-    );
+      );
 }
 
 export default Contact;
