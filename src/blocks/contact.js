1287827729897cf0dd3c39b95e837e66f8cd4a81
Kyron <kds060902@outlook.com>
2024-09-09 15:42:29 +0100

    further changes to contact block

--- src/blocks/contact.js
+++ src/blocks/contact.js
@@ -17,29 +17,31 @@ const findItemNames = (children) => {
     return itemNames;
 };
 
-
 const Contact = ({ attribs, children }) => {
-    const email = children.find(child => child.type === 'h2');
+    const email = findItemNames(children).join('').trim().replace(/\s+/g, '');
 
     const sectionId = React.useMemo(() => uuidv4(), []);
     const sectionRef = React.useRef(null);
+
+    console.log(findItemNames(children));
 
     return (
         <section className="contact-container" id={sectionId} ref={sectionRef}>
           <div className="contact-content">
             <h2 className="contact-h2">Contact Me</h2>
+            <p className="email">Please feel free to send over any questions or opportunities to <a href={`mailto:${email}`}>{email}</a>, or use the form below.</p>
             <form action="" method="POST">
               <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required />
+                <input type="text" id="name" name="name" placeholder="Enter Name" required />
               </div>
               <div>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
+                <input type="email" id="email" name="email" placeholder="Enter Email" required />
               </div>
               <div>
                 <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message" required></textarea>
+                <textarea id="message" name="message" placeholder="Enter Message" required></textarea>
               </div>
               <button type="submit">Send</button>
             </form>
